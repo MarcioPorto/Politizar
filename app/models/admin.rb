456a6943d2e1d9bc1e18c5f80c7f52343ee720e1
MarcioPorto <mporto@macalester.edu>
2018-03-30 17:22:22 -0600

    Added method to ensure no two super admins exist

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,13 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+
+  validate :save_admin?
+
+  private
+
+    def save_admin?
+      super_admins = Admin.where(super_admin: true)
+      errors.add(:super_admin, "can't have multiple super admins") if super_admins.length >= 1
+    end
 end
